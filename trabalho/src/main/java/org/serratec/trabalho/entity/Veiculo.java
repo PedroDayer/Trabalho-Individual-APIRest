e2fb701030ac7b10d7f7f5bfdac78f132dae0a70
Pedro Dayer <pedrob.dayer@gmail.com>
2026-05-13 08:39:29 -0300

    conexao com banco de dados + criançao e estruturacao das classes entity

---
+++ trabalho/src/main/java/org/serratec/trabalho/entity/Veiculo.java
@@ -0,0 +1,23 @@
+package org.serratec.trabalho.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+//@Entity
+//@Table
+public class Veiculo {
+
+    private UUID id;
+//    private Cliente cliente;
+    private String marca;
+    private String modelo;
+//    private
+//    private
+    private String placa;
+//    private
+//    private
+//    private
+//    private
+}
